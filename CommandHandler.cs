fb49934495bc24561453548acf67618cf6f04fa5
TFTWPhoenix <61560519+TFTWPhoenix@users.noreply.github.com>
2020-06-13 21:31:07 -0700

    Started dev on XCode
    Added print and printline to XCode and added CommandHandler.cs to handle the commands.

---
+++ CommandHandler.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Sys = Cosmos.System;
+
+namespace OS1
+{
+    public class CommandHandler
+    {
+
+        public static void exec(string cmd)
+        {
+            List<string> cmds = new List<string>();
+            cmds.Add("about");
+            cmds.Add("shutdown [-r]");
+            cmds.Add("edit");
+            cmds.Add("dir");
+            cmds.Add("rm");
+            cmds.Add("rmdir");
+            cmds.Add("mkdir");
+            cmds.Add("?/help/cmdlist");
+            string input;
+            if (cmd == "about")
+            {
+
+                Console.WriteLine("CommandOS Chipmunk is an operating system developed");
+                Console.WriteLine("using the Cosmos User Kit.");
+
+            }
+            else if (cmd.StartsWith("shutdown"))
+            {
+
+                if (cmd == "shutdown")
+                {
+
+                    Sys.Power.Shutdown();
+
+                }
+                else if (cmd == "shutdown -r")
+                {
+
+                    Sys.Power.Reboot();
+
+                }
+
+            }
+            else if (cmd == "edit")
+            {
+
+
+                Console.Write("Path to File: ");
+                input = Console.ReadLine();
+                if (File.Exists(input))
+                {
+
+                    Console.WriteLine("Old Contents: ");
+                    int i = 0;
+                    while (i < File.ReadAllLines(input).Length)
+                    {
+
+                        Console.WriteLine(File.ReadAllLines(input)[i]);
+                        i++;
+
+                    }
+                    Console.WriteLine("New Contents:");
+                    List<string> nconts = new List<string>();
+                    string ci = "";
+                    while (ci != @"\%exit")
+                    {
+
+                        ci = Console.ReadLine();
+                        if (ci != @"\%exit")
+                        {
+
+                            nconts.Add(ci);
+
+                        }
+
+
+
+                    }
+                    File.WriteAllLines(input, nconts.ToArray());
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Doesn't exist. Creating...");
+                    Console.WriteLine("Contents: ");
+                    List<string> nconts = new List<string>();
+                    string ci = "";
+                    while (ci != @"\%exit")
+                    {
+
+                        ci = Console.ReadLine();
+                        if (ci != @"\%exit")
+                        {
+
+                            nconts.Add(ci);
+
+                        }
+
+
+
+                    }
+                    File.WriteAllLines(input, nconts.ToArray());
+
+                }
+
+
+            }
+            else if (cmd == "dir")
+            {
+
+                Console.Write("Directory Of: ");
+                input = Console.ReadLine();
+                if (input != "")
+                {
+
+                    string[] dirdirs = Directory.GetDirectories(input);
+                    int i = 0;
+                    while (i < dirdirs.Length)
+                    {
+
+                        Console.WriteLine(dirdirs[i] + "    [DIR]");
+                        i++;
+
+                    }
+                    i = 0;
+                    string[] dirfils = Directory.GetFiles(input);
+                    while (i < dirfils.Length)
+                    {
+
+                        Console.WriteLine(dirfils[i] + "    [FILE]");
+                        i++;
+
+                    }
+
+                }
+                else
+                {
+
+                    string[] dirdirs = Directory.GetDirectories(@"0:\");
+                    int i = 0;
+                    while (i < dirdirs.Length)
+                    {
+
+                        Console.WriteLine(dirdirs[i] + "    [DIR]");
+                        i++;
+
+                    }
+                    i = 0;
+                    string[] dirfils = Directory.GetFiles(@"0:\");
+                    while (i < dirfils.Length)
+                    {
+
+                        Console.WriteLine(dirfils[i] + "    [FILE]");
+                        i++;
+
+                    }
+
+                }
+
+            }
+            else if (cmd == "rm")
+            {
+
+                Console.Write("Path to File: ");
+                input = Console.ReadLine();
+                if (input != "" && File.Exists(input) && input != @"0:\os\user.cfg")
+                {
+
+                    File.Delete(input);
+
+                }
+                else if (input == "")
+                {
+
+                    Console.WriteLine("Error: No file specified.");
+
+                }
+                else if (!File.Exists(input))
+                {
+
+                    Console.WriteLine("Error: File not found.");
+
+                }
+                else if (input == @"0:\os\user.cfg")
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Beep();
+                    Console.Write("[SysProtect] ");
+                    Console.ResetColor();
+                    Console.Write("This is a protected system file!");
+                    Console.WriteLine("");
+
+                }
+
+            }
+            else if (cmd == "rmdir")
+            {
+
+                Console.Write("Path to Directory: ");
+                input = Console.ReadLine();
+                if (input != "" && Directory.Exists(input))
+                {
+
+                    Directory.Delete(input);
+
+                }
+
+            }
+            else if (cmd == "mkdir")
+            {
+
+
+                Console.Write("Path (Including New Directory):  ");
+                input = Console.ReadLine();
+                if (input != "")
+                {
+                    Directory.CreateDirectory(input);
+                }
+
+            }
+            else if (cmd == "?" || cmd == "help" || cmd == "cmdlist")
+            {
+
+                int i = 0;
+                while (i < cmds.ToArray().Length)
+                {
+
+                    Console.WriteLine(cmds.ToArray()[i]);
+                    i++;
+
+                }
+
+            }
+            else if (cmd.StartsWith("clr"))
+            {
+
+                if (cmd.Contains("-m") || cmd.Contains("--multiclear"))
+                {
+
+                    Console.Clear();
+                    Console.Clear();
+                    Console.Clear();
+                    Console.Clear();
+
+                }
+
+                Console.Clear();
+
+            } else if (cmd.StartsWith("invoke "))
+            {
+
+                string toBeSearched = "invoke ";
+                int ix = cmd.IndexOf(toBeSearched);
+
+                if (ix != -1)
+                {
+                    string prog = cmd.Substring(ix + toBeSearched.Length);
+                    int i = 0;
+                    string[] lns = File.ReadAllLines(prog);
+                    while(i < lns.Length)
+                    {
+
+                        XCode.CodeHandler.exec(lns[i]);
+                        i++;
+
+                    }
+                }
+
+            }
+
+
+
+
+            else if (cmd == "")
+            {
+
+
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("Unknown command.");
+
+            }
+
+
+
+
+
+        }
+
+    }
+
+}
---
+++ XCode/XCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XCode
+{
+    public class CodeHandler
+    {
+
+        public static void exec(string code)
+        {
+
+            if(code.StartsWith("print "))
+            {
+
+                string toBeSearched = "print ";
+                int ix = code.IndexOf(toBeSearched);
+
+                if (ix != -1)
+                {
+                    string str = code.Substring(ix + toBeSearched.Length);
+                    Console.Write(str);
+                }
+
+            }
+            else if (code.StartsWith("printline "))
+            {
+
+                string toBeSearched = "printline ";
+                int ix = code.IndexOf(toBeSearched);
+
+                if (ix != -1)
+                {
+                    string str = code.Substring(ix + toBeSearched.Length);
+                    Console.WriteLine(str);
+                }
+
+            }
+
+        }
+
+    }
+}
